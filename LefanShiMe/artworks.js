135247bcabd0360c5f9ea8431ce131d592634793
Ben <bxc3201@rit.edu>
2019-03-02 14:12:20 -0600

    lefanshime

---
+++ LefanShiMe/artworks.js
@@ -0,0 +1,50 @@
+var artworks = [
+{
+  previewName: "Animal Cell <br> &amp <br> Plant Cell",
+  previewURL: "animal_cell_and_plant_cell_lefan_shi.jpg",
+  fullImageURL: "animal_cell_and_plant_cell_format.svg",
+  description: "Adobe Illustrator<br>Animal Cell and Plant Cell"
+},
+{
+  previewName: "Disease Poster: <br>Allergic Reactions",
+  previewURL: "allergic_reactions_lefan_shi.jpg",
+  fullImageURL: "allergic_reactions_format.svg",
+  description: "TODO Descripton Goes Here"
+},
+{
+  previewName: "Anatomical Diagram: <br>Facial Nerve",
+  previewURL: "facial_nerve_lefan_shi.jpg",
+  fullImageURL: "facial_nerve_format.svg",
+  description: "TODO Descripton Goes Here"
+},
+{
+  previewName: "Anatomical Diagram: <br>Brachial Plexus",
+  previewURL: "brachial_plexus_lefan_shi.jpg",
+  fullImageURL: "brachial_plexus_format.svg",
+  description: "TODO Descripton Goes Here"
+},
+{
+  previewName: "Botanical<br>Illustrations",
+  previewURL: "botanical_illustrations_lefan_shi.jpg",
+  fullImageURL: "rosa_chinensis_format.svg",
+  description: "TODO Descripton Goes Here"
+},
+{
+  previewName: "Papilionidae<br>Metamorphosis",
+  previewURL: "papilionidae_metamorphosis_lefan_shi.jpg",
+  fullImageURL: "papilionidae_metamorphosis_format.svg",
+  description: "Adobe Photoshop<br>Papilionidae Metamorphosis"
+},
+{
+  previewName: "Editorial Illustrations:<br>Robot Smoker",
+  previewURL: "robot_smoker_lefan_shi.jpg",
+  fullImageURL: "robot_smoker_format.svg",
+  description: "TODO Descripton Goes Here"
+},
+{
+  previewName: "Social Issue Poster:<br>Drug Abuse",
+  previewURL: "drug_abuse_lefan_shi.jpg",
+  fullImageURL: "drug_abuse_format.svg",
+  description: "TODO Descripton Goes Here"
+}
+];
