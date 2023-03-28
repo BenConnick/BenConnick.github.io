2f35542097961e2eda860682ee60a70bd8f05249
Ben <bxc3201@rit.edu>
2023-03-28 09:33:26 -0500

    new website

--- stickyClicker.js
+++
@@ -1,8 +0,0 @@
-
-var stickyNote = document.getElementById("sticky");
-stickyNote.addEventListener("click", playMuseum);
-
-function playMuseum() {
-    document.getElementById("uIn").focus();
-    stickyNote.parentNode.removeChild(stickyNote);
-}
